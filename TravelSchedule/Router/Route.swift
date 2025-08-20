8a564417c210d137b12245ba8e94b123a3b3a49c
Rodion Kim <skyrood2@yandex.ru>
2025-08-20 17:06:02 +0900

    added external router to manage routing across the app

---
+++ TravelSchedule/Router/Route.swift
@@ -0,0 +1,13 @@
+//
+//  Route.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/20.
+//
+
+import Foundation
+
+enum Route: Hashable {
+    case settlement(kind: SelectionKind)
+    case station(settlement: Settlement, kind: SelectionKind)
+}
---
+++ TravelSchedule/Router/Router.swift
@@ -0,0 +1,38 @@
+//
+//  Router.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/20.
+//
+
+import SwiftUI
+
+@Observable
+final class Router {
+    var path = NavigationPath()
+    
+    func go(to route: Route) {
+        path.append(route)
+    }
+//    
+//    func pop() {
+//        path.removeLast()
+//    }
+    
+    func pop(_ count: Int = 1) {
+        for _ in 0..<count where !path.isEmpty {
+            path.removeLast()
+        }
+    }
+    
+    func popToRoot() {
+        path = NavigationPath()
+    }
+    
+    func setPath(_ routes: [Route]) {
+        path = NavigationPath()
+        routes.forEach {
+            path.append($0)
+        }
+    }
+}
