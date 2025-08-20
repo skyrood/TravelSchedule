3e7323e2ff6a27e10d9409e23c85a377cc9ad27f
Rodion Kim <skyrood2@yandex.ru>
2025-08-20 17:10:00 +0900

    added global navigation to the app, added builder and viewModel as environment objects

--- TravelSchedule/TravelScheduleApp.swift
+++ TravelSchedule/TravelScheduleApp.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 @main
 struct TravelScheduleApp: App {
+    @State private var router = Router()
+    @State private var builder = TripBuilder()
+    @State private var viewModel = SettlementViewModel()
+    
     var body: some Scene {
         WindowGroup {
 //            ContentView()
-            RootView()
+            NavigationStack(path: $router.path) {
+                RootView()
+                .navigationDestination(for: Route.self, destination: destination)
+            }
+            .environment(router)
+            .environment(builder)
+            .environment(viewModel)
+        }
+    }
+    
+    @ViewBuilder
+    private func destination(_ route: Route) -> some View {
+        switch route {
+        case .settlement(let kind):
+            SettlementSelectionView(kind: kind)
+        case .station(let settlement, let kind):
+            StationSelectionView(settlement: settlement, kind: kind)
         }
     }
 }
