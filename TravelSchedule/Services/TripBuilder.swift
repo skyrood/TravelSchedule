683c1a273fa7815aa37dd990d9c3870be6d4ce27
Rodion Kim <skyrood2@yandex.ru>
2025-08-20 17:05:36 +0900

    Added TripBuilder environment class to manage the start point and destination

---
+++ TravelSchedule/Services/TripBuilder.swift
@@ -0,0 +1,48 @@
+//
+//  TripBuilder.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/20.
+//
+
+import Observation
+
+enum SelectionKind { case from, to }
+
+struct PlaceSelection: Equatable {
+    var settlement: Settlement?
+    var station: Station?
+}
+
+@Observable final class TripBuilder {
+    var from = PlaceSelection()
+    var to = PlaceSelection()
+    
+    func setSettlement(settlement: Settlement, for kind: SelectionKind) {
+        switch kind {
+        case .from:
+            from.settlement = settlement
+            from.station = nil
+        case .to:
+            to.settlement = settlement
+            to.station = nil
+        }
+    }
+    
+    func setStation(station: Station, for kind: SelectionKind) {
+        switch kind {
+        case .from: from.station = station
+        case .to:   to.station   = station
+        }
+    }
+    
+    var isReady: Bool {
+        from.settlement != nil && from.station != nil &&
+        to.settlement   != nil && to.station   != nil
+    }
+    
+    func routeParams() -> (from: Station, to: Station)? {
+        guard let from = from.station, let to = to.station else { return nil }
+        return (from, to)
+    }
+}
