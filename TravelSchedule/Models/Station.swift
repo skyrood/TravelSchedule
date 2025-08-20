d8f6f716d74b0167129a5c4deb98f6d5b1c9beaa
Rodion Kim <skyrood2@yandex.ru>
2025-08-20 17:06:18 +0900

    added models for Settlement and Station

---
+++ TravelSchedule/Models/Station.swift
@@ -0,0 +1,13 @@
+//
+//  Station.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/19.
+//
+
+import Foundation
+
+struct Station: Identifiable, Hashable {
+    var id = UUID()
+    var name: String
+}
