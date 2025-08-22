4a809177c89bde49f93e6f7fd1d3e2b90d7ae28f
Rodion Kim <skyrood2@yandex.ru>
2025-08-22 18:16:40 +0900

    Changed Service model to support yandex schedule timeDate format

--- TravelSchedule/Models/Service.swift
+++ TravelSchedule/Models/Service.swift
@@ -12,8 +12,45 @@ struct Service: Identifiable, Hashable {
     let companyName: String
     let companyLogo: String
     let transferInfo: String?
-    let date: String
-    let departureTime: String
-    let arrivalTime: String
-    let duration: String
+    let departureTime: Date
+    let arrivalTime: Date
+    
+    var departureHHmm: String {
+        TimeFormatters.time.string(from: departureTime)
+    }
+    var arrivalHHmm: String {
+        TimeFormatters.time.string(from: arrivalTime)
+    }
+    
+    var departureDate: String {
+        TimeFormatters.dayMonth.string(from: departureTime)
+    }
+    
+    var hasTransfers: Bool {
+        transferInfo != nil && !(transferInfo?.isEmpty ?? true)
+    }
+    
+    var durationText: String {
+        let mins = max(0, Int(arrivalTime.timeIntervalSince(departureTime) / 60))
+        let h = mins / 60
+        let m = mins % 60
+        if h > 0 && m > 0 { return "\(h) ч \(m) мин" }
+        if h > 0          { return "\(h) ч" }
+        return "\(m) мин"
+    }
+}
+
+enum TimeFormatters {
+    static let time: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm"
+        return formatter
+    }()
+    
+    static let dayMonth: DateFormatter = {
+        let f = DateFormatter()
+        f.locale = Locale(identifier: "ru_RU")
+        f.dateFormat = "d MMMM"   // пример: "14 января"
+        return f
+    }()
 }
