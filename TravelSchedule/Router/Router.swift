ef9e18898acea23d72d47a43a1849dd1951048e3
Rodion Kim <skyrood2@yandex.ru>
2025-08-22 21:30:32 +0900

    Modified Router to support tab routes

--- TravelSchedule/Router/Router.swift
+++ TravelSchedule/Router/Router.swift
@@ -9,26 +9,66 @@ import SwiftUI
 
 @Observable
 final class Router {
-    var path = NavigationPath()
+    var selectedTab: AppTabRoute = .schedule
+    
+    var schedulePath = NavigationPath()
+    var settingsPath = NavigationPath()
+    
+    func pathBinding(for tab: AppTabRoute) -> Binding<NavigationPath> {
+        Binding(
+            get: { [unowned self] in
+                switch tab {
+                case .schedule: return self.schedulePath
+                case .settings: return self.settingsPath
+                }
+            },
+            set: { [unowned self] newValue in
+                switch tab {
+                case .schedule: self.schedulePath = newValue
+                case .settings: self.settingsPath = newValue
+                }
+            }
+        )
+    }
     
     func go(to route: Route) {
-        path.append(route)
+        schedulePath.append(route)
+    }
+    
+    func go(to route: SettingsRoute) {
+        settingsPath.append(route)
+    }
+    
+    func go(to tab: AppTabRoute, route: Route) {
+        selectedTab = tab
+        DispatchQueue.main.async { self.schedulePath.append(route) }
+    }
+    
+    func go(to tab: AppTabRoute, route: SettingsRoute) {
+        selectedTab = tab
+        DispatchQueue.main.async { self.settingsPath.append(route) }
     }
     
     func pop(_ count: Int = 1) {
-        for _ in 0..<count where !path.isEmpty {
-            path.removeLast()
+        switch selectedTab {
+        case .schedule:
+            for _ in 0..<count where !schedulePath.isEmpty { schedulePath.removeLast() }
+        case .settings:
+            for _ in 0..<count where !settingsPath.isEmpty { settingsPath.removeLast() }
         }
     }
     
-    func popToRoot() {
-        path = NavigationPath()
+    func popToRoot(_ tab: AppTabRoute? = nil) {
+        switch tab ?? selectedTab {
+        case .schedule: schedulePath = NavigationPath()
+        case .settings: settingsPath = NavigationPath()
+        }
     }
     
     func setPath(_ routes: [Route]) {
-        path = NavigationPath()
+        schedulePath = NavigationPath()
         routes.forEach {
-            path.append($0)
+            schedulePath.append($0)
         }
     }
 }
---
+++ TravelSchedule/Router/Routes/SettingsRoute.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsRoute.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/22.
+//
+
+import Foundation
+
+enum SettingsRoute: Hashable {
+    case connectionError
+    case serverError
+}
