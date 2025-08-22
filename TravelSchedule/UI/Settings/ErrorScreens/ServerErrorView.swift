d56775adc7b4d95ebbe0245732b340d40c3a4e3b
Rodion Kim <skyrood2@yandex.ru>
2025-08-22 21:24:29 +0900

    Added ConnectionErrorView and ServerErrorView

---
+++ TravelSchedule/UI/Settings/ErrorScreens/ServerErrorView.swift
@@ -0,0 +1,34 @@
+//
+//  ServerErrorView.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/08/22.
+//
+
+import SwiftUI
+
+struct ServerErrorView: View {
+    var body: some View {
+        ZStack {
+            Color.ypWhite.ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Image(.serverError)
+                    .resizable()
+                    .frame(width: 223, height: 223)
+                    .clipped()
+                    .scaledToFit()
+                Text("Ошибка сервера")
+                    .font(.bold24)
+                    .foregroundColor(.ypBlack)
+                    .padding(.top, 16)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    ServerErrorView()
+}
