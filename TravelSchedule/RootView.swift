bc80c2ebd3bce26d430f3df39e9898ba0f46b68a
Rodion Kim <skyrood2@yandex.ru>
2025-08-23 15:49:43 +0900

    added launchscreen to the app; set the splash screen showing time to 0.5s

--- TravelSchedule/RootView.swift
+++ TravelSchedule/RootView.swift
@@ -19,7 +19,7 @@ struct RootView: View {
             }
         }
         .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                 withAnimation {
                     dataLoading = false
                 }
