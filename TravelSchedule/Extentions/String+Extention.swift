3f24b00b7ee22a012e1449f9702884cfbc345efe
Rodion Kim <skyrood2@yandex.ru>
2025-09-24 03:21:54 +0900

    added String extention for processing dateTime strings

---
+++ TravelSchedule/Extentions/String+Extention.swift
@@ -0,0 +1,30 @@
+//
+//  String+Extention.swift
+//  TravelSchedule
+//
+//  Created by Rodion Kim on 2025/09/24.
+//
+
+import Foundation
+
+extension String {
+    var timeValue: Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        return formatter.date(from: self)
+    }
+    
+    var hourComponent: Int? {
+        let parts = self.split(separator: ":")
+        if let hourString = parts.first, let hour = Int(hourString) {
+            return hour
+        }
+        return nil
+    }
+    
+    var dateValue: Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter.date(from: self)
+    }
+}
